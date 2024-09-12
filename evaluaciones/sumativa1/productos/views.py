3755bce0ba24578002db19b6e83af2e9d1a66719
Tamara Valenzuela <tamara.valenzuela08@inacapmail.cl>
2024-09-12 00:09:51 -0300

    funciona T-T

--- evaluaciones/sumativa1/productos/views.py
+++ evaluaciones/sumativa1/productos/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
 
-# Create your views here.
-def index(request):
-    return HttpResponse ("hola")
+productos_registrados = []
+
+def registro(request):
+    if request.method == 'POST':
+        nombre = request.POST.get('nombre')
+        codigo = request.POST.get('codigo')
+        marca = request.POST.get('marca')
+        fecha = request.POST.get('fecha')
+
+        # Crear un diccionario que represente el producto y añadirlo a la lista
+        producto = {
+            'nombre': nombre,
+            'codigo': codigo,
+            'marca': marca,
+            'fecha': fecha,
+        }
+        productos_registrados.append(producto)
+
+        # Redirigir a la lista de productos después de registrar uno
+        return redirect('consulta')
+
+    return render(request, 'registro.html')
+
+def consulta(request):
+    # Pasar la lista de productos registrados a la plantilla
+    return render(request, 'consulta.html', {'productos': productos_registrados})
