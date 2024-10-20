0e98a4b987a5077b39fe588d94ebda17a0ee8ff7
Tamara Valenzuela <tamara.valenzuela08@inacapmail.cl>
2024-10-20 01:39:34 -0300

    modelos, views y superuser

--- evaluaciones/sumativa1/productos/models.py
+++ evaluaciones/sumativa1/productos/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Marca(models.Model):
+    id_marca = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+class Categoria(models.Model):
+    id_categoria = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+class Caracteristicas(models.Model):
+    id_caracteristica = models.AutoField(primary_key=True)
+    tamano = models.IntegerField()
+    peso = models.IntegerField()
+
+class Producto(models.Model):
+    codigo = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    precio = models.IntegerField()
+    marca = models.ForeignKey(Marca, null=True, on_delete=models.CASCADE)
+    categoria = models.ForeignKey(Categoria, null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre +' '+ str(self.marca) 
--- evaluaciones/sumativa1/productos/views.py
+++ evaluaciones/sumativa1/productos/views.py
@@ -1,26 +1,45 @@
 from django.shortcuts import render, redirect
+from .models import Producto, Marca,Caracteristicas, Categoria
 
-productos_registrados = []
+def index(request):
+    all_productos = Producto.objects.all().order_by('codigo')
+    
+    productos_actuales = []
+    for producto in all_productos:
+        producto_actual = {
+            'codigo': producto.codigo,
+            'nombre': producto.nombre,
+            'precio' : producto.precio,
+            'marca' : producto.marca,
+            'categoria' : producto.categoria,
+        }
+        productos_actuales.append(producto_actual)
+    
+    context = { 'productos': productos_actuales }
+    return render(request, 'index.html', context)
+    
+
 
 def registro(request):
     if request.method == 'POST':
-        nombre = request.POST.get('nombre')
         codigo = request.POST.get('codigo')
-        marca = request.POST.get('marca')
-        fecha = request.POST.get('fecha')
+        nombre = request.POST.get('nombre')
+        precio = request.POST.get('precio')
+        marca_id = request.POST.get('marca_id')
+        categoria_id = request.POST.get('categoria_id')
 
-        producto = {
-            'nombre': nombre,
-            'codigo': codigo,
-            'marca': marca,
-            'fecha': fecha,
-        }
-        productos_registrados.append(producto)
+        marca = Marca.objects.get(id_marca = marca_id) 
+        categoria = Categoria.objects.get(id_categoria = categoria_id) 
 
-        return redirect('consulta')
+        producto = Producto (
+            codigo = codigo,
+            nombre = nombre,
+            precio = precio,
+            marca = marca,
+            categoria = categoria
+        )
+        producto.save()
 
-    return render(request, 'registro.html')
-
-def consulta(request):
-
-    return render(request, 'consulta.html', {'productos': productos_registrados})
+        return redirect(index)
+    else:
+        return render (request,'registro.html')
