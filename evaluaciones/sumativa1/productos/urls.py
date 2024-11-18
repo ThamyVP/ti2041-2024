75f89fcb3565363901f44eef0194e9143d560c06
Tamara Valenzuela <tamara.valenzuela08@inacapmail.cl>
2024-11-18 16:50:52 -0300

    evaluacion 3

--- evaluaciones/sumativa1/productos/urls.py
+++ evaluaciones/sumativa1/productos/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name= 'index'),
-    path('registro/', views.registro, name='registro'),
+    path('', views.login_view, name='login'),  
+    path('productos/', views.index, name='lista_productos'),
+    path('productos/registro/', views.registro, name='registro'),  
+    path('logout/', views.logout_view, name='logout'),
 ]
+
+    
--- evaluaciones/sumativa1/productos/views.py
+++ evaluaciones/sumativa1/productos/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render, redirect
 from .models import Producto, Marca,Caracteristicas, Categoria
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from django.contrib.auth.decorators import user_passes_test, login_required
+from django.contrib.auth import logout
+from django.utils.timezone import now
 
+
+@login_required
 def index(request):
     all_productos = Producto.objects.all().order_by('codigo')
     
@@ -17,9 +24,8 @@ def index(request):
     
     context = { 'productos': productos_actuales }
     return render(request, 'index.html', context)
-    
 
-
+@login_required
 def registro(request):
     if request.method == 'POST':
         codigo = request.POST.get('codigo')
@@ -43,3 +49,49 @@ def registro(request):
         return redirect(index)
     else:
         return render (request,'registro.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            # Iniciar sesión
+            login(request, user)
+
+            #variables de sesión
+            request.session['username'] = user.username
+            request.session['login_date'] = now().strftime('%Y-%m-%d %H:%M:%S')  # Formato legible
+            request.session['grupo'] = user.groups.filter(name='ADMIN_PRODUCTS').exists()
+
+            return redirect('lista_productos')
+        else:
+            messages.error(request, 'Usuario o clave incorrectos')
+
+    return render(request, 'login.html')
+
+@login_required
+def lista_productos(request):
+    all_productos = Producto.objects.all().order_by('codigo')
+    
+    productos_actuales = []
+    for producto in all_productos:
+        producto_actual = {
+            'codigo': producto.codigo,
+            'nombre': producto.nombre,
+            'precio' : producto.precio,
+            'marca' : producto.marca,
+            'categoria' : producto.categoria,
+        }
+        productos_actuales.append(producto_actual)
+    
+    return render(request, 'productos/consulta.html', {'productos': productos_actuales})
+
+def logout_view(request):
+    request.session.flush()
+    logout(request)
+    return redirect('/')
+
+def grupo(user):
+    return user.groups.filter(name='grupo').exists()
